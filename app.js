cd3815dbc63ac3248108d18b8a424bf819da5a9e
Debanjan Das <microsoft2420@gmail.com>
2025-01-18 10:05:40 +0530

    New feature1

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-import { Text }  from "./randomText";
+import { Text }  from "./RandomText.js";
 let result = document.querySelector(".result");
 let para = document.querySelector(".para");
 let userinputBox = document.querySelector(".userinp");
 let genbtn = document.querySelector(".generate");
-let dispTimer = document.querySelector(".timerinto");
+let timer = document.querySelector("#timer");
+let timerCount = document.querySelector(".start");
 let data;
 let totalWords = 0;
-let arr = [];
+let textKey = 0 ;
 
-const genpara = () => {
+function generateTxt() {
   userinputBox.classList.remove("noshow");
-  let textKey = Math.floor(Math.random() * 9) + 1;
+  textKey = Math.floor(Math.random() * 9);
   console.log(textKey);
-  data = Text.textKey ;
-  console.log(data);
   genbtn.classList.add("noshow");
-  // wordChecker(data);
-};
+  data = Text[textKey] ;
+  console.log(data);
+  para.textContent = data;
+  wordChecker(data) ;
+  const date = new Date();
+  const timeZone = date;
+  console.log(timeZone);  
+
+}
+genbtn.addEventListener("click", generateTxt);
+
 
 const wordChecker = (text) => {
   const wordsArray = text.split(/\s+/); 
@@ -25,35 +33,51 @@ const wordChecker = (text) => {
   console.log(`Total words: ${totalWords}`);
 };
 
-//Timer for user (actually not timer it is a stopwatch for now!)
-let timer = null;
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false;
-
+// Timer for user
 const start = () => {
-  if (!isRunning) {
-    startTime = Date.now() - elapsedTime;
-    timer = setInterval(update, 10);
-    console.log(timer);
-    isRunning = true;
-  }
-};
-const stop = () => {
-  alert("HELLO THERE!!");
-};
-const reset = () => {};
-const update = () => {
-  const currenTime = Date.now();
-  elapsedTime = currenTime - startTime;
+  let sec = 0 ;
+  let min = 0 ;
+  let currenTime = 0 ;
+  currenTime = setInterval(() => {
+    sec++ ;
+    if(sec === 60){
+      sec = 0 ;
+      min++ ;
+    }
+    timer.textContent = `${min} : ${sec}`;
+  }, 1000);
+}
 
-  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-  let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
-  let seconds = Math.floor((elapsedTime / 1000) % 60);
+timerCount.addEventListener("click", start);
+// //Timer for user (actually not timer it is a stopwatch for now!)
+// let timer = null;
+// let startTime = 0;
+// let elapsedTime = 0;
+// let isRunning = false;
 
-  hours = String(hours).padStart(2, "0");
-  minutes = String(minutes).padStart(2, "0");
-  seconds = String(seconds).padStart(2, "0");
+// const start = () => {
+//   if (!isRunning) {
+//     startTime = Date.now() - elapsedTime;
+//     timer = setInterval(update, 10);
+//     console.log(timer);
+//     isRunning = true;
+//   }
+// };
+// const stop = () => {
+//   alert("HELLO THERE!!");
+// };
+// const reset = () => {};
+// const update = () => {
+//   const currenTime = Date.now();
+//   elapsedTime = currenTime - startTime;
 
-  dispTimer.textContent = `${hours}:${minutes}:${seconds}`;
-};
+//   let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+//   let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+//   let seconds = Math.floor((elapsedTime / 1000) % 60);
+
+//   hours = String(hours).padStart(2, "0");
+//   minutes = String(minutes).padStart(2, "0");
+//   seconds = String(seconds).padStart(2, "0");
+
+//   dispTimer.textContent = `${hours}:${minutes}:${seconds}`;
+// };
