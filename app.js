80d2652baa7d1e296f8d40a9700a0412471af8c3
Debanjan Das <microsoft2420@gmail.com>
2025-01-22 08:32:03 +0530

    New added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let timerCountStart = document.querySelector(".start");
 let timerCountStop = document.querySelector(".stop");
 let userInputText = document.querySelector("#userinput");
 let data;
+let userReset = document.querySelector(".reset");
 let totalWords = 0;
 let userInputedWords = 0;
 let textKey = 0;
@@ -20,6 +21,7 @@ let currenTime;
 function generateTxt() {
   userinputBox.classList.remove("noshow");
   textKey = Math.floor(Math.random() * 15);
+  console.log(textKey);
   genbtn.classList.add("noshow");
   data = Text[textKey];
   para.textContent = data;
@@ -36,7 +38,7 @@ const wordChecker = (text) => {
   const wordsArray = text.split(/\s+/); 
   const filteredWords = wordsArray.filter((word) => word.length > 0); 
   userInputedWords = filteredWords.length;
-  console.log(`Total words: ${userInputedWords}`);
+  // console.log(`Total words: ${userInputedWords}`);
 };
 
 // Timer for user
@@ -57,10 +59,10 @@ const start = () => {
 };
 
 const stop = () => {
-  console.log("STOP");
+  // console.log("STOP");
   clearInterval(currenTime);
-  console.log(min);
-  console.log(sec);
+  // console.log(min);
+  // console.log(sec);
   userInputText.disabled = true;
   speedCheck();
 };
@@ -73,7 +75,7 @@ function speedCheck() {
     alert("Time's up!");
     wordChecker(userInputText.value);
     let second = min * 60 + sec; // 1 * 60 + 0 this will happen
-    console.log(second);
+    // console.log(second);
     if (second > 0) {
       let speedWPM = Math.floor((userInputedWords * 60) / second);
       console.log(`Speed: ${speedWPM} WPM`);
@@ -82,3 +84,13 @@ function speedCheck() {
     }
   }
 }
+
+const reset = () => {
+  userInputText.value = "";
+  para.textContent = "";
+  genbtn.classList.remove("noshow");
+  clearInterval(currenTime);
+  timer.innerText = "00:00";
+  userInputText.disabled = true;
+}
+userReset.addEventListener("click", reset);
